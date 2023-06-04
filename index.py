0ca9b58f0b4ff0212ff7928f905efcdc8c31664b
Loïc <loic.schussler@epita.fr>
2023-06-04 11:32:27 +0200

    feat(main): api + interface

--- index.py
+++ index.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file,  send_from_directory
 import os
 from imageScanner.main import process_image
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    #add style.css to index.html
-    return render_template('index.html')
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+
+# there is only one value in the request (filename : string)
+@app.post("/process")
+def process():
+    filename = request.form['filename']
+
+    print("filename: ", filename)
+    absolute_path = os.path.abspath(filename)
+    print("absolute_path: ", absolute_path)
+    currentDirectory = os.getcwd()
+    savePath = currentDirectory + 'processed.xml'
+    process_image(absolute_path, savePath)
+    
+    return send_file(savePath, as_attachment=True)
 
 @app.route('/upload', methods=['POST'])
 def upload():    # get file from request formData
@@ -18,13 +33,9 @@ def upload():    # get file from request formData
     with open(os.path.join(filename), 'wb') as f:
         f.write(file.read())
 
-    absolute_path = os.path.abspath(filename)
-    print("absolute_path: ", absolute_path)
-    currentDirectory = os.getcwd()
-    savePath = currentDirectory + 'processed.xml'
-    process_image(absolute_path, savePath)
 
-    return send_file(savePath, as_attachment=True)
+    return "success"
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
